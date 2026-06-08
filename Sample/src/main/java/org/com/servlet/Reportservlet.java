70b4982ee4b24f4e6179e917a2524df57a470010
Vinodh <gowdavinod995@gmail.com>
2026-06-08 22:15:53 +0530

    Created Many Api's

--- Sample/src/main/java/org/com/servlet/Reportservlet.java
+++ Sample/src/main/java/org/com/servlet/Reportservlet.java
@@ -9,15 +9,28 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-//@WebServlet("/hello")
+@WebServlet("/hello/*")
 public class Reportservlet extends HttpServlet{
 	
 	@Override
 	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException,ServletException {
 		res.setContentType("text/html");
 		
-		PrintWriter out = res.getWriter();
-		out.println("<h1>Welcome to Servlet World....</h1>");
+//		PrintWriter out = res.getWriter();
+//		out.println("<h1>Welcome to Servlet World....</h1>");
+		
+		String path=req.getPathInfo();
+		
+		
+		if("/report".equals(path)) {
+			PrintWriter out = res.getWriter();
+//			out.println("<h1>Welcome to Report Servlet....</h1>");
+			res.getWriter().write("Welcome to Report Servlet....");
+		}
+		else if("/free".equals(path)) {
+			PrintWriter out = res.getWriter();
+			res.getWriter().write("Welcome to Free Servlet....");
+		}
 		
 		System.out.println("Reportservlet doGet method called");
 	}
