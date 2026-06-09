82408e1dd9f36de548f6d7dad6903fe629ac6577
Vinodh <gowdavinod995@gmail.com>
2026-06-09 23:21:02 +0530

    CRUD Operation done with Servlet

--- Sample/src/main/java/org/com/servlet/Reportservlet.java
+++ Sample/src/main/java/org/com/servlet/Reportservlet.java
@@ -3,8 +3,11 @@ package org.com.servlet;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import jakarta.servlet.ServletException;
@@ -16,38 +19,73 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/hello/*")
 public class Reportservlet extends HttpServlet{
 	
-	@Override
-	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException,ServletException {
-		res.setContentType("text/html");
-		PrintWriter writer=res.getWriter();
-		 Gson gson=new Gson();
-         BufferedReader reader = req.getReader();
-         StringBuilder json = new StringBuilder();
-         String line;
-         
-         while((line=reader.readLine())!=null) { //Read the request one by one like {,name,vinod,}
-        	 json.append(line);//add the value to the line object
-         }
-         
-         JsonObject obj=gson.fromJson(json.toString(), JsonObject.class);
-         
-         String name=obj.get("name").getAsString();
-         int age=obj.get("age").getAsInt();
-         
+	private static Map<Integer, JsonObject> inmemory=new HashMap();
+	Gson gson=new Gson();
+	
+//	@Override
+//	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException,ServletException {
+//		res.setContentType("text/html");
+//		PrintWriter writer=res.getWriter();
+//		 Gson gson=new Gson();
+//         BufferedReader reader = req.getReader();
+//         StringBuilder json = new StringBuilder();
+//         String line;
+//         
+//         while((line=reader.readLine())!=null) { //Read the request one by one like {,name,vinod,}
+//        	 json.append(line);//add the value to the line object
+//         }
+//         
+//         JsonObject obj=gson.fromJson(json.toString(), JsonObject.class);
+//         
+//         String name=obj.get("name").getAsString();
+//         int age=obj.get("age").getAsInt();
+//         
+//         JsonArray arr=obj.getAsJsonArray("address");
+//         
 //         JsonObject response=new JsonObject();
 //         response.addProperty("Name", name);
 //         response.addProperty("Age", age);
-         
-         String xml="<employee>"
-         		+ "<name>"+name+"</name>"
-         		+ "<age>"+age+"</age>"
-         		+ "</employee>";
-        		 
-        		 
-         
-         writer.print(xml);
-         
-         
+//         response.add("Address", arr);
+////         
+////         String xml="<employee>"
+////         		+ "<name>"+name+"</name>"
+////         		+ "<age>"+age+"</age>"
+////         		+ "</employee>";
+//        		 
+//        		 
+//         
+//         writer.print(response);
+//         
+	//Performing CRUD operation
+         @Override
+         protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+        	        throws ServletException, IOException {
+                PrintWriter wri=resp.getWriter();
+        	    JsonObject obj = gson.fromJson(req.getReader(), JsonObject.class);
 
-}
+        	    int id = obj.get("id").getAsInt();
+
+        	    inmemory.put(id, obj);
+        	    wri.print("Data is Created");
+       }
+       @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	   PrintWriter wri=resp.getWriter();
+    	   int id=Integer.parseInt(req.getParameter("id"));
+    	   JsonObject ret=inmemory.get(id);
+    	   
+    	   if(ret!=null) {
+    		   wri.print(ret);
+    	   }
+    	   else {
+    		   wri.print("User not Found");
+    	   }
+    }
+       @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	PrintWriter wir=resp.getWriter();
+    	   int id=Integer.parseInt(req.getParameter("id"));
+    	inmemory.remove(id);
+    	wir.print("User is removed");
+    }
 }
